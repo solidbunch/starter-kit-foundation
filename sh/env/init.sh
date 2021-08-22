35e246adc0bf5398eaabf607713a183e49975f96
Yurii Pavlov <mailyurik@gmail.com>
2021-08-22 23:46:42 +0300

    improve environment_type env files names, update readme

--- sh/env/init.sh
+++ sh/env/init.sh
@@ -15,11 +15,11 @@ if [ "$1" ]; then
 fi
 
 # Check .env.${ENVIRONMENT_TYPE} file exist
-if [ ! -f .env."${ENVIRONMENT_TYPE}" ]; then
-    echo "[Error] .env.${ENVIRONMENT_TYPE} file not found in $PWD"; exit 1;
+if [ ! -f .env.type."${ENVIRONMENT_TYPE}" ]; then
+    echo "[Error] .env.type.${ENVIRONMENT_TYPE} file not found in $PWD"; exit 1;
 fi
 
 # Run secrets generator and make .env.secret file
 #bash sh/env/secret-gen.sh
 
-cat .env.main <(echo) .env."${ENVIRONMENT_TYPE}" <(echo) .env.secret > .env
+cat .env.main <(echo) .env.type."${ENVIRONMENT_TYPE}" <(echo) .env.secret > .env
