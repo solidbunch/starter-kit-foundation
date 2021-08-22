9499f076ce40d25eaf143cff3e8104baa56dbd35
Yurii Pavlov <mailyurik@gmail.com>
2021-08-22 22:13:42 +0300

    testing env init script

--- Makefile
+++ Makefile
@@ -1,11 +1,2 @@
-SHELL := /bin/bash
-WP_ENVIRONMENT_TYPE=development
-
 up:
-	@bash run-docker-compose.sh -e .env -e .env.development -e .env.local -f docker-compose.yml -f docker-compose.proxy.yml $(type)
-
-down:
-	bash run-docker-compose.sh -e .env -e .env.development -e .env.local down
-
-test:
-	test
+	docker-compose up -d
---
+++ sh/env/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script path
+SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+
+# Project root directory
+cd $(dirname "$(readlink -f "$0")")/../../ || exit 1
+
+# Default values
+ENVIRONMENT_TYPE=dev
+
+# Parse environment type args
+if [ "$1" ]; then
+    ENVIRONMENT_TYPE="$1"
+fi
+
+# Check .env.${ENVIRONMENT_TYPE} file exist
+if [ ! -f .env."${ENVIRONMENT_TYPE}" ]; then
+    echo "[Error] .env.${ENVIRONMENT_TYPE} file not found in $PWD"; exit 1;
+fi
+
+# Run secrets generator and make .env.secret file
+bash sh/env/secret-gen.sh
+
+cat .env.main <(echo) .env."${ENVIRONMENT_TYPE}" <(echo) .env.secret > .env
