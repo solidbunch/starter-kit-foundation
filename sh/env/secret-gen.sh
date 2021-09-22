d6c6d5727e5a2d91f2d37dbcc2b339427a9c2dd3
Yurii Pavlov <mailyurik@gmail.com>
2021-09-22 23:02:36 +0300

    make  cross platform password generator

--- sh/env/secret-gen.sh
+++ sh/env/secret-gen.sh
@@ -16,15 +16,20 @@ if [ ! -f ./sh/env/.env.secret.template ]; then
     echo -e "${LIGHTRED}[Error]${WHITE} .env.secret.template file not found in ./sh/env/${NOCOLOR}"; exit 1;
 fi
 
-# Generate secrets, copy .env.secret template to .env.secret and replace generated secrets
-awk '
-  /generatethispass/ {
-    cmd = "< /dev/urandom tr -dc A-Za-z0-9_ | head -c 20"
-    cmd | getline str
-    close(cmd)
-    gsub("generatethispass", str)
-  }
-  { print }
-' ./sh/env/.env.secret.template > ./config/environment/.env.secret
+# Check pass-gen.sh file exist
+if [ ! -f ./sh/env/pass-gen.sh ]; then
+    echo -e "${LIGHTRED}[Error]${WHITE} pass-gen.sh file not found in ./sh/env/${NOCOLOR}"; exit 1;
+fi
+
+# Cross platform password generator. Creating Alpine container, running awk, removing container
+echo -e "${CYAN}[Info]${WHITE} Creating Alpine container, running awk, removing container${NOCOLOR}"
+
+docker run -it --name pass-gen-container -d alpine
+docker cp ./sh/env/.env.secret.template pass-gen-container:/.env.secret.template
+docker cp ./sh/env/pass-gen.sh pass-gen-container:/pass-gen.sh
+docker exec -it pass-gen-container sh /pass-gen.sh
+docker cp pass-gen-container:/.env.secret ./config/environment/.env.secret
+docker rm -f pass-gen-container
+
 
 echo -e "${LIGHTGREEN}[Success]${WHITE} Secrets done in ./config/environment/.env.secret${NOCOLOR}"
