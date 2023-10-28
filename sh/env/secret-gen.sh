f1a1a364e80351e29c02a0002b7b1db5dce57bf2
Yurii Pavlov <mailyurik@gmail.com>
2023-10-28 18:21:08 +0200

    update wp-config and secrets generation; fix -it error in pipelines

--- sh/env/secret-gen.sh
+++ sh/env/secret-gen.sh
@@ -8,7 +8,7 @@ source ./sh/utils/colors
 
 # Check .env.secret file exist
 if [ -f ./config/environment/.env.secret ]; then
-    echo -e "${CYAN}[Info]${NOCOLOR} .env.secret file already exist in ./config/environment/"; exit;
+    echo -e "${CYAN}[Info]${NOCOLOR} .env.secret file already exist in ./config/environment/"; exit 0;
 fi
 
 # Check .env template file exist
@@ -16,20 +16,11 @@ if [ ! -f ./sh/env/.env.secret.template ]; then
     echo -e "${LIGHTRED}[Error]${NOCOLOR} .env.secret.template file not found in ./sh/env/"; exit 1;
 fi
 
-# Check pass-gen.sh file exist
-if [ ! -f ./sh/env/pass-gen.sh ]; then
-    echo -e "${LIGHTRED}[Error]${NOCOLOR} pass-gen.sh file not found in ./sh/env/"; exit 1;
-fi
+# Cross platform password generator. Creating Alpine container, running pass gen, removing container
+echo -e "${CYAN}[Info]${NOCOLOR} Generating Secrets..."
 
-# Cross platform password generator. Creating Alpine container, running awk, removing container
-echo -e "${CYAN}[Info]${NOCOLOR} Creating Alpine container, running awk, removing container"
-
-docker run -it --name pass-gen-container -d alpine
-docker cp ./sh/env/.env.secret.template pass-gen-container:/.env.secret.template
-docker cp ./sh/env/pass-gen.sh pass-gen-container:/pass-gen.sh
-docker exec -it pass-gen-container sh /pass-gen.sh
+docker run --name pass-gen-container -v "./sh:/shell:ro" -it alpine:latest sh /shell/env/pass_gen.sh
 docker cp pass-gen-container:/.env.secret ./config/environment/.env.secret
-docker rm -f pass-gen-container
-
+docker rm -f pass-gen-container > /dev/null 2>&1
 
 echo -e "${LIGHTGREEN}[Success]${NOCOLOR} Secrets done in ./config/environment/.env.secret"
