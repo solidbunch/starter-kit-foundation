3f1347c101c7190b6e114c932ff87c78d21a6399
Yurii Pavlov <mailyurik@gmail.com>
2024-02-07 21:54:33 +0100

    php image ready, pushed to github

--- dockerfiles/php/docker-entrypoint.sh
+++ dockerfiles/php/docker-entrypoint.sh
@@ -14,17 +14,23 @@ replace_env_vars() {
 
     if [ -f "$template_file" ]; then
         envsubst < "$template_file" > "$output_file"
-        echo "Processed $template_file"
+        echo "sSMTP processed $template_file"
     else
         echo "Warning: File $template_file does not exist. Skipping."
     fi
 }
 
+# Recreate www-data user
+# Fix www-data UID from 82 to ${CURRENT_UID} (Permission denied error)
+deluser www-data; \
+addgroup -g "${CURRENT_GID}" www-data; \
+adduser -u "${CURRENT_UID}" -D -G www-data www-data
+
+echo "www-data user UID=${CURRENT_UID} updated"
+
 # Replace env variables with values in sSMTP config using gettext app
 replace_env_vars "/etc/ssmtp/templates/ssmtp.conf.template" "/etc/ssmtp/ssmtp.conf"
 replace_env_vars "/etc/ssmtp/templates/revaliases.template" "/etc/ssmtp/revaliases"
-
-echo "sSMTP config processing complete"
 
 ## exec php-fpm (added as parameter in Dockerfile CMD ["php-fpm"])
 exec "$@"
