d2f4aec57fb7f0f10ff59ad9c643e9cc12f4d845
Yurii Pavlov <mailyurik@gmail.com>
2024-02-18 12:35:21 +0100

    update job deploy action
    update install in build job action
    
    update
    
    update
    
    update
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test1
    
    update deploy actions
    
    update deploy action exclude
    
    deploy branches update

--- sh/install.sh
+++ sh/install.sh
@@ -8,15 +8,21 @@ source ./sh/utils/colors
 
 source ./.env
 
-echo -e "${CYAN}[Info]${NOCOLOR} Installing project with \
-WP_DEFAULT_THEME ${LIGHTYELLOW}'${WP_DEFAULT_THEME}'${NOCOLOR}, \
-WP_ENVIRONMENT_TYPE ${LIGHTYELLOW}'${WP_ENVIRONMENT_TYPE}'${NOCOLOR}, \
-and APP_BUILD_MODE ${LIGHTYELLOW}'${APP_BUILD_MODE}'${NOCOLOR}";
+if [ "$1" ]; then
+    CONFIRMED="$1"
+fi
 
-read -rp "Are you sure? (y/n): " choice
-if [[ ! $choice =~ ^[Yy]$ ]]; then
-  echo "Not confirmed. Exiting."
-  exit 0
+echo -e "${CYAN}[Info]${NOCOLOR} Installing project with \
+WP_DEFAULT_THEME ${LIGHTYELLOW}'$WP_DEFAULT_THEME'${NOCOLOR}, \
+WP_ENVIRONMENT_TYPE ${LIGHTYELLOW}'$WP_ENVIRONMENT_TYPE'${NOCOLOR}, \
+and APP_BUILD_MODE ${LIGHTYELLOW}'$APP_BUILD_MODE'${NOCOLOR}";
+
+if [ "$CONFIRMED" != "yes" ]; then
+  read -rp "Are you sure? (y/n): " choice
+  if [[ ! $choice =~ ^[Yy](es)?$ ]]; then
+    echo "Not confirmed. Exiting."
+    exit 0
+  fi
 fi
 
 # Build main docker images
@@ -27,10 +33,16 @@ docker compose -f docker-compose.build.yml build
 
 # Run composer scripts
 # Use composer update for update to last changes without lock file if we are using custom dependency. For example custom theme or plugin with 'dev-branch-name' version
-docker compose -f docker-compose.build.yml run -it --rm --build composer su -c "composer update-${APP_BUILD_MODE}" "${DEFAULT_USER}"
+# Use composer install for install from lock file for regular cases for theme or plugin
+docker compose -f docker-compose.build.yml run --rm composer su -c "\
+    composer update-${APP_BUILD_MODE} && \
+    cd /srv/web/wp-core && \
+    wp core verify-checksums && \
+    cd /srv/web/wp-content/themes/${WP_DEFAULT_THEME} && \
+    composer install-${APP_BUILD_MODE}" \
+  "${DEFAULT_USER}"
 
-# Use composer install for install from lock file for regular cases
-docker compose -f docker-compose.build.yml run -it --rm --build composer su -c "cd web/wp-content/themes/${WP_DEFAULT_THEME} && composer install-${APP_BUILD_MODE}" "${DEFAULT_USER}"
-
-# Run node scripts
-docker compose -f docker-compose.build.yml run -it --rm --build node su -c "npm run install-${APP_BUILD_MODE} --prefix ./wp-content/themes/${WP_DEFAULT_THEME}" "${DEFAULT_USER}"
+# Run Node scripts
+docker compose -f docker-compose.build.yml run --rm node su -c "\
+    npm run install-${APP_BUILD_MODE} --prefix ./wp-content/themes/${WP_DEFAULT_THEME}" \
+  "${DEFAULT_USER}"
