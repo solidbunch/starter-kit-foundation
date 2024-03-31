8146126b102adf720b4aced4f0caa0c230c7ff91
Yurii Pavlov <mailyurik@gmail.com>
2024-04-01 00:34:05 +0200

    test theme node cache

--- sh/install.sh
+++ sh/install.sh
@@ -41,8 +41,3 @@ docker compose -f docker-compose.build.yml run --rm composer su -c "\
     cd /srv/web/wp-content/themes/${WP_DEFAULT_THEME} && \
     composer install-${APP_BUILD_MODE}" \
   "${DEFAULT_USER}"
-
-# Run Node scripts
-docker compose -f docker-compose.build.yml run --rm node su -c "\
-    npm run install-${APP_BUILD_MODE} --prefix ./wp-content/themes/${WP_DEFAULT_THEME}" \
-  "${DEFAULT_USER}"
