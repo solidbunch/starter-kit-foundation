36dc0933e7bf54e98d1ccaf4479e521b4a4ceea8
Yurii Pavlov <mailyurik@gmail.com>
2024-07-24 18:01:46 +0200

    watch browsersync fix

--- sh/npm-watch.sh
+++ sh/npm-watch.sh
@@ -9,6 +9,6 @@ source ./sh/utils/colors
 source ./.env
 
 # Run watch in theme folder
-docker compose -f docker-compose.build.yml run --rm node su -c "\
+docker compose -f docker-compose.build.yml run --service-ports --rm node su -c "\
     npm run watch --prefix ./wp-content/themes/${WP_DEFAULT_THEME}" \
   "${DEFAULT_USER}"
