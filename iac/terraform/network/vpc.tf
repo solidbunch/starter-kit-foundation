1c9d51eebe353c21f9b5b204f2ed7823e43dcdf6
Yurii Pavlov <mailyurik@gmail.com>
2025-04-23 23:17:08 +0100

    update terraform network

--- iac/terraform/network/vpc.tf
+++ iac/terraform/network/vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main_vpc" {
-  cidr_block                     = "10.0.0.0/16"
+  cidr_block                       = "10.0.0.0/16"
   assign_generated_ipv6_cidr_block = true
-  enable_dns_support             = true
-  enable_dns_hostnames           = true
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
 
   tags = {
     Name = "main_vpc"
