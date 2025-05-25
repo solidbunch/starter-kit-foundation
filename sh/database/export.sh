751043bf7ac21a33ac2fd3b1e35ee131e9957651
Yurii Pavlov <mailyurik@gmail.com>
2025-05-25 23:20:26 +0100

    fix export file name

--- sh/database/export.sh
+++ sh/database/export.sh
@@ -25,12 +25,14 @@ else
   OUTPUT_FILE="$DATABASE_CONTAINER"-"$MYSQL_DATABASE"-"$ENVIRONMENT_TYPE"-"$APP_DOMAIN"-$(date +%Y-%m-%d).sql
 fi
 
+CONTAINER_OUTPUT_FILE="/tmp/$DATABASE_CONTAINER-$MYSQL_DATABASE-$ENVIRONMENT_TYPE-$APP_DOMAIN-$(date +%Y-%m-%d).sql"
+
 echo "Exporting local database to '${OUTPUT_FILE}'. It can take more than a few minutes. Please wait."
 
 # Export data to sql file
-docker compose exec "${DATABASE_CONTAINER}" bash -c "mariadb-dump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} > /tmp/${OUTPUT_FILE}"
+docker compose exec "${DATABASE_CONTAINER}" bash -c "mariadb-dump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} > ${CONTAINER_OUTPUT_FILE}"
 
 # Copy dump from container
-docker compose cp "${DATABASE_CONTAINER}":/tmp/"${OUTPUT_FILE}" "${OUTPUT_FILE}"
+docker compose cp "${DATABASE_CONTAINER}":"${CONTAINER_OUTPUT_FILE}" "${OUTPUT_FILE}"
 
 echo -e "${LIGHTGREEN}[Success]${NOCOLOR} Database export done"
